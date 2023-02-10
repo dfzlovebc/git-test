0c07370cf59661f2bd8d10290110f5d72b9309bc
Layne <Layne@atguigu.com>
2023-02-10 11:55:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * 名称:GitTest
+ * 描述:
+ * Package:com.atguigu.git
+ *
+ * @version 1.0
+ * @Data:2023-02-1011:52
+ * @Author:1597714468@qq.com
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
